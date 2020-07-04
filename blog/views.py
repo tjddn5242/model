5e97313e70763c6f8594665c4307ac56d02669a9
sungwoo <tjddn5242@gmail.com>
2020-07-04 22:04:40 +0900

    first commit of Model

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Blog
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs': blogs})
+
+def bootstrap(request):
+    return render(request, 'bootstrap.html')
